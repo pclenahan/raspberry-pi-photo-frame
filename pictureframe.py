0aa59ce6056ec89abd6854101b73d3404e236295
Paul Clenahan <paul@clenahan.com>
2019-02-03 12:25:46 -0800

    Cleaned up code, added comments and followed python conventions

--- pictureframe.py
+++ pictureframe.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
-#
+"""
+Loop through images in a directory tree and display each like a picture frame
+
+Author:        Paul Clenahan
+Original Date: Feb 3, 2019
+"""
 
 import argparse
 import os
@@ -8,47 +13,64 @@ import sys
 import time
 import random
 import pygame
-from pygame.locals import QUIT, KEYDOWN, K_ESCAPE
 
 def get_image_file(top):
+    """
+    Pick a random image filename from directory structure.
+
+    Arguments:
+    top -- Top level folder to start from
+
+    Based on "Picking a Random Line from a File" algorithm from Perl Cookbook
+    by Nathan Torkington, Tom Christiansen. This approach avoids having to
+    read the whole directory structure (which has an unknown number of files)
+    into memory. Since the structure is read each time, also supports files
+    being added to the structure while the program is running.
+    """
 
     random.seed()
 
-    for attempts in range(10):   # Try up to 10 times to find an image
-        n=0
-        for root, dirs, files in os.walk(top):
-            for name in files:
+    n=0
+    full_name = "None"
+
+    for root, dirs, files in os.walk(top):
+        for name in files:
+            current_name = os.path.join(root, name)
+            file_name, file_ext = os.path.splitext(current_name)
+            # We only care about images, so don't count other files
+            if file_ext in (".JPG",".jpg", ".PNG", ".png"):
                 n=n+1
                 if random.uniform(0, n) < 1:
-                    full_name = os.path.join(root, name)
-                    file_name, file_ext = os.path.splitext(full_name)
-        if file_ext in (".JPG",".jpg", ".PNG", ".png"):
-            break                # If we found an image we are done
-        else:
-            full_name = "None"
+                    full_name = current_name
 
     print("    Image file: ", full_name)
 
     return full_name
 
 def correct_orientation(image):
+    """
+    Checks EXIF information and rotates image accordingly.
+
+    Arguments:
+    image -- The pygame image object
+    """
 
     # See if EXIF data is present
     try:
-        OrientationTag = str(tags["Image Orientation"])
+        orientation_tag = str(tags["Image Orientation"])
     except:
-        OrientationTag = "None"
+        orientation_tag = "None"
         print("    No EXIF data found - No changes to orientation made")
         return image
         
     # Rotate if necessary based on EXIF data    
-    if (OrientationTag == "Rotated 90 CW"):
+    if (orientation_tag == "Rotated 90 CW"):
         print("    Correcting for EXIF Rotated 90 CW")
         image = pygame.transform.rotate(image, -90)
-    elif (OrientationTag == "Rotated 90 CCW"):   
+    elif (orientation_tag == "Rotated 90 CCW"):   
         print("    Correcting for EXIF Rotated 90 CCW")
         image = pygame.transform.rotate(image, 90)
-    elif (OrientationTag == "Rotated 180"):   
+    elif (orientation_tag == "Rotated 180"):   
         print("    Correcting for EXIF Rotated 180")
         image = pygame.transform.rotate(image, 180)
     else:
@@ -57,27 +79,35 @@ def correct_orientation(image):
     return image
 
 def scale_image(display_width, display_height, image):
+    """
+    Scale pygame image up/down to fit display size.
 
-    current_width = image.get_width()
-    current_height = image.get_height()
+    Arguments:
+    display_height -- Vertical screen size (in whole pixels)
+    display_width -- Horizontal screen size (in whole pixels)
+    image -- The pygame image object
+    """
 
-    print("    Original width : ", current_width)
-    print("    Original height: ", current_height)
+    original_width = image.get_width()
+    original_height = image.get_height()
 
-    # Prepare Image by Scaling it up/down as needed to fit the screen
+    print("    Original width : ", original_width)
+    print("    Original height: ", original_height)
 
-    width_ratio = display_width / current_width
-    height_ratio = display_height / current_height
+    # Find ratios of image to screen
+
+    width_ratio = display_width / original_width
+    height_ratio = display_height / original_height
 
     if (height_ratio <= width_ratio):
         # Height is the constraining dimension, so scale up/down based on that
-        new_width = int(current_width * height_ratio)
-        new_height = int(current_height * height_ratio)
+        new_width = int(original_width * height_ratio)
+        new_height = int(original_height * height_ratio)
         image = pygame.transform.smoothscale(image, (new_width, new_height))
     else:
         # Width is the constraining dimension, so scale up/down based on that
-        new_width = int(current_width * width_ratio)
-        new_height = int(current_height * width_ratio)
+        new_width = int(original_width * width_ratio)
+        new_height = int(original_height * width_ratio)
         image = pygame.transform.smoothscale(image, (new_width, new_height))
 
     print("    Scaled width : ", new_width)
@@ -85,15 +115,24 @@ def scale_image(display_width, display_height, image):
 
     return image
 
-def cleanUpAndExit():
+def cleanup_and_exit():
+    """
+    Reset pygame display so screen works correctly and exit
+    """
     pygame.mouse.set_visible(True)
     pygame.quit()
     sys.exit(1)
 
-##### Real work starts here #####
-
 def main(top_path, frame_mode, display_time):
+    """
+    Main picture frame loop that does the real work.
 
+    Arguments:
+    top_path -- Top directory structure to look for images in
+    frame_mode -- "Landscape", "Portrait", "All" determines what images to show
+    display_time -- How many seconds to display each image 
+    """
+    
     print("----- Setting things up -----")
     
     # Test for image support
@@ -109,31 +148,34 @@ def main(top_path, frame_mode, display_time):
     display_width = display_info.current_w
     display_height = display_info.current_h
 
-    modes = pygame.display.list_modes()
-    pygame.display.set_mode(max(modes), pygame.FULLSCREEN)
+    pygame.display.set_mode((display_width,display_height), pygame.FULLSCREEN)
+
     pygame.display.set_caption("Picture Frame")
     pygame.mouse.set_visible(False)
 
     screen = pygame.display.get_surface()
 
-    print("Detected display resolution is: ", modes)
+    print("Detected display resolution is:")
     print("    Width : ", display_width)
     print("    Height: ", display_height)
 
     # Done setting up the display screen, now we go into main image loop
 
     keep_looping = True
+    n = 0
     
     while keep_looping:
 
+        n = n+1
+
         print(" ")
-        print("----- Iteration: ", iter, " -----")
+        print("----- Iteration: ", n, " -----")
 
         image_file = get_image_file(top_path)
         
         if image_file == "None":
             print("Could not find any images to display. Exiting.")
-            cleanUpAndExit()
+            cleanup_and_exit()
 
         image = pygame.image.load(image_file)
 
@@ -149,13 +191,14 @@ def main(top_path, frame_mode, display_time):
         if (frame_mode == "All") or (image_type == frame_mode):
             image = scale_image(display_width, display_height, image)
             screen.fill((0,0,0))
-            screen.blit(image, ((display_width - image.get_width() )/2,(display_height-image.get_height())/2))
+            screen.blit(image, ((display_width - image.get_width() )/2,
+                                (display_height-image.get_height())/2))
             pygame.display.flip()
             time.sleep(display_time)
         else:
             print("    Image skipped since we are in ", frame_mode, " mode")
 
-        # Check for any key being pressed to exit while loop.
+        # Check for any key presses and handle accordingly
 
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
@@ -171,9 +214,14 @@ def main(top_path, frame_mode, display_time):
   
         # Hey, we are done - let"s reset display and exit
 
-    cleanUpAndExit()
+    cleanup_and_exit()
+
+# Let's get started...
 
 if __name__ == "__main__":
+
+    # Grab any command line arguments
+    
     parser = argparse.ArgumentParser(
         description="Displays images for a picture frame"
     )
@@ -202,10 +250,13 @@ if __name__ == "__main__":
         help="Amount of time (in seconds) to display each image.",
         dest="display_time"
     )
+
     args = parser.parse_args()
+    
     print("----- Welcome to Picture Frame -----")
-    print("Path: ", args.top_path)
-    print("Mode: ", args.frame_mode)
-    print("Time: ", args.display_time)
+    print("Input arguments are:")
+    print("    Path: ", args.top_path)
+    print("    Mode: ", args.frame_mode)
+    print("    Time: ", args.display_time)
 
     main(args.top_path, args.frame_mode, args.display_time)
